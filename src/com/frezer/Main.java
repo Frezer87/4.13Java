e9958707e5f31d65adad5d2389f29695f0b56f0e
Frezer Gebremeskel <frezer.kidane@gmail.com>
2018-01-25 13:35:53 -0500

    Initial commit

---
+++ src/com/frezer/Main.java
@@ -0,0 +1,16 @@
+package com.frezer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double deg = 47;
+
+        System.out.println(deg + " degrees is " + Math.toRadians(deg) + " radians.");
+
+        double A = Math.toRadians(deg);
+
+        System.out.println( " A is an available of " + A );
+
+    }
+}
